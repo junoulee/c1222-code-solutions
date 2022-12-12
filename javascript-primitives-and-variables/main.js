29159b1a1e7e59fa778cbc7b88557aac350926d2
Junou Lee <junoulee@gmail.com>
2022-12-12 17:45:19 +0000

    javascript-primitives-and-variables - Junou Lee

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,18 @@
+var fullName;
+fullName = 'Junou Lee';
+var isCool = true;
+var totalPets;
+totalPets = 2;
+var unknown = null;
+var never;
+
+console.log('My Full Name:', fullName);
+console.log('value of isCool:', isCool);
+console.log('value of totalPets:', totalPets);
+console.log('typeof fullName:', typeof 'Junou Lee');
+console.log('typeof isCool:', typeof true);
+console.log('typeof totalPets', typeof 2);
+console.log('value of unknown:', unknown);
+console.log('typeof unknown:', typeof unknown);
+console.log('value of never:', never);
+console.log('typeof never:', typeof never);
