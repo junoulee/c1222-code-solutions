3d767b13ca61ddabbe4e8ccdfb22cc143680937e
Junou Lee <junoulee@gmail.com>
2022-12-13 00:14:02 +0000

    javascript-arrays - Junou Lee

---
+++ javascript-arrays/main.js
@@ -0,0 +1,20 @@
+var colors = ['red', 'white', 'blue'];
+
+console.log('value of colors[0]:', colors[0]);
+console.log('value of colors[1]:', colors[1]);
+console.log('value of colors[2]:', colors[2]);
+console.log('America is ' + colors[0] + ', ' + colors[1] + ', and', colors[2] + '.');
+
+colors[2] = 'green';
+console.log('Mexico is ' + colors[0] + ', ' + colors[1] + ', and', colors[2] + '.');
+
+console.log('value of colors array:', colors);
+
+var students = ['Homer', 'Bart', 'Lisa', 'Marge'];
+var numberOfStudents = students.length;
+console.log('There are ' + numberOfStudents + ' students in the class.');
+var lastIndex = numberOfStudents - 1;
+lastIndex = students[3];
+var lastStudent = lastIndex;
+console.log('The last student in the array is ' + lastStudent + '.');
+console.log('value of students:', students);
