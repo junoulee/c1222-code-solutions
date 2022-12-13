e161208c9f4e4dac296f3e4e4db31b3e9ee3f1be
Junou Lee <junoulee@gmail.com>
2022-12-13 18:14:03 +0000

    javascript-data-model - Junou Lee

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,53 @@
+var orderHistory = [
+  {
+    order_number: ['114-3941689-8772232'],
+    order_name: ['JavaScript for impatient programmers'],
+    order_placed: ['August 4, 2020'],
+    order_delivered: ['August 8, 2020'],
+    return_window: ['Sep 7, 2020'],
+    total_price: ['$34.00'],
+    listed_price: ['$31.55'],
+    author: ['Rauschmayer, Dr. Axel'],
+    ship_to: ['JS Masher'],
+    delivery: [null]
+
+  },
+  {
+    order_number: ['113-9984268-1280257'],
+    order_name: ['The Timeless Way of Building'],
+    order_placed: ['July 19, 2020'],
+    order_delivered: ['July 20, 2020'],
+    return_window: ['Aug 19, 2020'],
+    total_price: ['$44.53'],
+    listed_price: ['$41.33'],
+    author: ['Alexander, Christopher'],
+    ship_to: ['JS Masher'],
+    delivery: ['directly to resident']
+
+  },
+  {
+    order_number: ['114-2875557-9059409'],
+    order_name: ['Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for Wii U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter'],
+    order_placed: ['July 4, 2020'],
+    order_delivered: ['July 7, 2020'],
+    return_window: ['Aug 5, 2020'],
+    total_price: ['$17.22'],
+    listed_price: ['$15.98'],
+    author: [null],
+    ship_to: ['JS Masher'],
+    delivery: ['directly to resident']
+  },
+  {
+    order_number: ['113-2883177-2648248', '113-2883177-2648248'],
+    order_name: ['Gamecube Controller - Super Smash Bros. Edition (Nintendo Switch)', 'The Art of Sql'],
+    order_placed: ['July 3, 2020', 'July 3, 2020'],
+    order_delivered: ['July 5, 2020', 'July 5, 2020'],
+    return_window: ['Aug 4, 2020', 'Aug 4, 2020'],
+    total_price: ['138.93', '138.93'],
+    listed_price: [("' controller = $94.95' + book = '$33.99'"), ("' controller = $94.95' + book = '$33.99'")],
+    author: [null, 'Faroult, Stephane'],
+    ship_to: ['JS Masher', 'JS Masher'],
+    delivery: [null, null]
+  }
+
+];
