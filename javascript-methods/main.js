962d0b76fc7e2462d4b84ef0d898778471645e1c
Junou Lee <junoulee@gmail.com>
2022-12-14 20:02:27 +0000

    javascript-methods - Junou Lee

---
+++ javascript-methods/main.js
@@ -0,0 +1,39 @@
+var a = 3;
+var b = 5;
+var c = 7;
+
+var maximumValue = Math.max(a, b, c);
+console.log('maximumValue:', maximumValue);
+
+var heroes = ['Batman', 'Superman', 'Spiderman', 'Venom'];
+var randomNumber = Math.random();
+randomNumber = randomNumber * heroes.length;
+var randomIndex = Math.floor(randomNumber);
+console.log('randomIndex:', randomIndex);
+
+var library = [{ title: 'Outliers', author: 'Gladwell' }, { title: '1984', author: 'Orwell' }, { title: 'Farenheit 451', author: 'Bradbury' }];
+var lastBook = library.pop();
+console.log('lastBook:', lastBook);
+var firstBook = library.shift();
+console.log('firstBook:', firstBook);
+
+var js = {
+  title: 'JavaScript for Impatient Programmers',
+  author: 'Dr. Axel Rauschmayer'
+};
+var css = {
+  title: 'CSS Secrets',
+  author: 'Lea Verou'
+};
+
+library.push(js);
+library.unshift(css);
+library.splice(1, 1);
+console.log('value of the library:', library);
+
+var fullName = 'Junou Lee';
+var firstAndLastName = fullName.split(' ');
+console.log(firstAndLastName);
+var firstName = firstAndLastName[0];
+var sayMyName = firstName.toUpperCase();
+console.log('sayMyName:', sayMyName);
