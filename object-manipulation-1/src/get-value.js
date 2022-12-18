e79d2703afe02d9c79805dc43cedc606ca8758ff
Junou Lee <junoulee@gmail.com>
2022-12-18 09:24:07 +0000

    simplified first solution

--- object-manipulation-1/src/get-value.js
+++ object-manipulation-1/src/get-value.js
@@ -1,12 +1,7 @@
 /* exported getValue */
-// do for in loop to find key in the object (var property in object)
-// if key is found in object, return value of property
+// bracket notation object[key]
 
 function getValue(object, key) {
 
-  for (var property in object) {
-    if (key === property) {
-      return object[property];
-    }
-  }
+  return object[key];
 }
