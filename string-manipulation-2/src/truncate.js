93541c9ea81706c5bb1331fd0e68d3cfe9889e8a
Junou Lee <junoulee@gmail.com>
2022-12-19 17:04:42 +0000

    not done

---
+++ string-manipulation-2/src/truncate.js
@@ -0,0 +1,11 @@
+/* exported truncate */
+// make a variable for an empty string
+// run for loop (i=0, i < string.length, i++)
+// in the code block - concatenate string.length with ellipses
+
+function truncate(length, string) {
+
+  var result = '';
+  result = string[length];
+  return result;
+}
