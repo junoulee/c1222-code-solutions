944b129b2f3db6b48d46c36dacea059e7bca8319
Junou Lee <junoulee@gmail.com>
2022-12-19 17:54:38 +0000

    dom-querying - Junou Lee

---
+++ dom-querying/main.js
@@ -0,0 +1,10 @@
+console.log('hello, world');
+
+console.log(document.querySelector('h1'));
+console.dir(document.location);
+console.log(document.querySelector('#explanation'));
+console.dir(document.location);
+console.log(document.querySelector('.hint'));
+console.dir(document.location);
+console.log(document.querySelectorAll('p'));
+console.log(document.querySelectorAll('.example-link'));
