87a1171e0964eec7f18420b03604d098d23f8a11
Junou Lee <junoulee@gmail.com>
2022-12-20 16:53:25 +0000

    revised

--- javascript-forms/form-values.js
+++ javascript-forms/form-values.js
@@ -2,9 +2,9 @@ function getData(event) {
   console.log('submit');
 
   event.preventDefault();
-  var one = document.forms[0].elements[0].name.value;
-  var two = document.forms[0].elements[1].name.value;
-  var three = document.forms[0].elements[2].name.value;
+  var one = xx.elements.name.value;
+  var two = xx.elements.name.value;
+  var three = xx.elements.name.value;
   var object = { name: one, email: two, message: three };
 
   console.log(object);
