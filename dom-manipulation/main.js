ec4912a738abda61336496717d914069db44c0c3
Junou Lee <junoulee@gmail.com>
2022-12-20 21:18:47 +0000

    better variable names

--- dom-manipulation/main.js
+++ dom-manipulation/main.js
@@ -6,24 +6,24 @@
 // make if statements that correlate to directions and update class names using x.className;
 var counter = 0;
 
-var x = document.querySelector('.hot-button');
-var y = document.querySelector('.click-count');
+var hotButton = document.querySelector('.hot-button');
+var clickCount = document.querySelector('.click-count');
 
 function click(event) {
 
-  y.textContent = 'Clicks: ' + counter++;
+  clickCount.textContent = 'Clicks: ' + counter++;
   if (counter < 4) {
-    x.className = 'hot-button cold';
+    hotButton.className = 'hot-button cold';
   } if (counter > 4 && counter < 7) {
-    x.className = 'hot-button cool';
+    hotButton.className = 'hot-button cool';
   } if (counter > 7 && counter < 13) {
-    x.className = 'hot-button tepid';
+    hotButton.className = 'hot-button tepid';
   } if (counter > 13 && counter < 16) {
-    x.className = 'hot-button warm';
+    hotButton.className = 'hot-button warm';
   } if (counter > 16) {
-    x.className = 'hot-button nuclear';
+    hotButton.className = 'hot-button nuclear';
   }
 
 }
 
-x.addEventListener('click', click, false);
+hotButton.addEventListener('click', click, false);
