a0701f1f4082ba75a3b2dd128ffeec74bfc0e1b5
Junou Lee <junoulee@gmail.com>
2022-12-21 19:22:11 +0000

    almost done

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -2,17 +2,18 @@
 // make function to go inside event listener
 // make event listener listen for click
 // need to make the background & button change colors whhen clicked.
-var counter = 0;
+var isOn = true;
 
 var click = document.querySelector('.button');
-var clickCount = document.querySelector('.click count');
 
-document.querySelector('body');
+var bodyVar = document.querySelector('body');
 
 function lightBulb(event) {
-  clickCount.className.style.backgroundColor = 'black';
-  return counter;
 
+  if (isOn === true) {
+    click.className = 'button-clicked';
+    bodyVar.className = 'body-black';
+  }
 }
 
 click.addEventListener('click', lightBulb);
