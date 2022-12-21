31b5e4f1fa0631977bf61435446d96a778b1a93e
Junou Lee <junoulee@gmail.com>
2022-12-21 17:31:47 +0000

    not done

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -1,12 +1,18 @@
-var bodyVar = document.querySelector('body');
+// set variables for body & buttom element
+// make function to go inside event listener
+// make event listener listen for click
+// need to make the background & button change colors whhen clicked.
+var counter = 0;
+
+var click = document.querySelector('.button');
+var clickCount = document.querySelector('.click count');
+
+document.querySelector('body');
 
 function lightBulb(event) {
+  clickCount.className.style.backgroundColor = 'black';
+  return counter;
 
-  var colors = ['111111', '#FEF9F1'];
-  for (var i = 0; i < colors.length; i++) {
-    bodyVar.style.backgroundColor = colors[i];
-  }
 }
-var click = document.querySelector('.button');
 
 click.addEventListener('click', lightBulb);
