62a0c8a49206bea9b3e1ff11f9bfa4ba427af5f2
Junou Lee <junoulee@gmail.com>
2023-01-04 19:16:25 +0000

    javascript-timers - Junou Lee

--- javascript-timers/timeout.js
+++ javascript-timers/timeout.js
@@ -1,2 +1,6 @@
-var dotDotDot = document.querySelector('.message');
-dotDotDot.textContent = 'Hello There';
+
+function helloThere() {
+  var dotDotDot = document.querySelector('.message');
+  dotDotDot.textContent = 'Hello There';
+}
+setTimeout(helloThere, 2000);
