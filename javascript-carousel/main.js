6efb297712c6bdafa5e6448de292b05c89654b7d
Junou Lee <junoulee@gmail.com>
2023-01-18 22:15:58 +0000

    not done

---
+++ javascript-carousel/main.js
@@ -0,0 +1,13 @@
+// var image = document.querySelector('img');
+
+// var phases = 5;
+// var auto = setInterval(function () {
+
+//   if (phases === 0) {
+
+//   } else {
+//     image.setAttribute('src', './images/001.png');
+
+//   }
+
+// }, 3000);
