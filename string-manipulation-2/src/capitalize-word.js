38fe53e49731795dc2ae01a603c075f9da8990cb
Junou Lee <junoulee@gmail.com>
2023-01-19 18:02:56 +0000

    fixed

--- string-manipulation-2/src/capitalize-word.js
+++ string-manipulation-2/src/capitalize-word.js
@@ -1,7 +1,7 @@
 /* exported capitalizeWord */
 // set variable result
 // lowercase each word (word.toLowerCase())
-// make if statement for JavaScript to uppercase indexes 0 and 4 and slice out unneeded letters; (for JavaScript)
+// make if statement for JavaScript to be returned if charAt === 's';
 // set result to toUpperCase index 0 and slice index 1
 
 function capitalizeWord(word) {
@@ -10,8 +10,8 @@ function capitalizeWord(word) {
   result = word.toLowerCase();
 
   if (result.charAt(4) === 's') {
-    result = result.charAt(0).toUpperCase() + result.slice(1, 4) + result.charAt(4).toUpperCase() + result.slice(5);
-    return result;
+
+    return 'JavaScript';
 
   }
   result = result.charAt(0).toUpperCase() + result.slice(1);
