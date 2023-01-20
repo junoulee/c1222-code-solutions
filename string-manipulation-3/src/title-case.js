9d4755cb8187f19405f5587355a3cbbdca255911
Junou Lee <junoulee@gmail.com>
2023-01-20 08:00:21 +0000

    not done

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,13 +1,42 @@
 /* exported titleCase */
 
 function titleCase(title) {
-  title = title.toLowerCase();
-  title = title.split(' ');
+  title = title.toLowerCase().split(' ');
+  var minorWords = ['And', 'Or', 'Nor', 'But', 'As', 'By', 'For', 'In', 'Of', 'On', 'Per', 'To'];
+  var hardCode = ['Javascript:', 'Javascript', 'Api'];
+
   for (var i = 0; i < title.length; i++) {
-    // if (title[i] === 'and' || title[i] === 'or' || title[i] === 'nor' || title[i] === 'but' || title[i] === 'as' || title[i] === 'by' || title[i] === 'for' || title[i] === 'in' || title[i] === 'of' || title[i] === 'on' || title[i] === 'per' || title[i] === 'to') {
-    //   title[i]
-    // }
+
     title[i] = title[i].charAt(0).toUpperCase() + title[i].slice(1);
+
   }
+
+  if (title.includes(hardCode[0]) === true) {
+    var indexOne = title.indexOf(hardCode[0]);
+    hardCode[0] = hardCode[0].slice(0, 4) + hardCode[0].charAt(4).toUpperCase() + hardCode[0].slice(5);
+    title.splice(indexOne, 1, hardCode[0]);
+  }
+
+  if (title.includes(hardCode[1]) === true) {
+    var indexTwo = title.indexOf(hardCode[1]);
+    hardCode[1] = hardCode[1].slice(0, 4) + hardCode[1].charAt(4).toUpperCase() + hardCode[1].slice(5);
+    title.splice(indexTwo, 1, hardCode[1]);
+
+  }
+
+  if (title.includes(hardCode[2]) === true) {
+    var indexThree = title.indexOf(hardCode[2]);
+    hardCode[2] = hardCode[2].slice(0, 1) + hardCode[2].charAt(1).toUpperCase() + hardCode[2].charAt(2).toUpperCase();
+    title.splice(indexThree, 1, hardCode[2]);
+  }
+
+  for (i = 0; i < minorWords.length; i++) {
+    if (title.includes(minorWords[i]) === true) {
+      var index = title.indexOf(minorWords[i]);
+      minorWords = minorWords[i].toLowerCase();
+      title.splice(index, 1, minorWords);
+    }
+  }
+
   return title.join(' ');
 }
