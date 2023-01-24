1a948b31ca756025831cc610dbcdf569f676b6af
Junou Lee <junoulee@gmail.com>
2023-01-24 22:21:23 +0000

    node-module-system - Junou Lee

---
+++ node-module-system/a-wrapped-module.js
@@ -0,0 +1,5 @@
+console.log('typeof exports:', exports);
+console.log('typeof require:', require);
+console.log('typeof module:', module);
+console.log('typeof __filename:', __filename);
+console.log('typeof __dirname:', __dirname);
