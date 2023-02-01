635d8888213460c324619eb950bc9a54c95ee284
Junou Lee <junoulee@gmail.com>
2023-02-01 23:07:09 +0000

    array-reduce - Junou Lee

---
+++ array-reduce/main.js
@@ -0,0 +1,37 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+const account = [
+  { type: 'deposit', amount: 150 },
+  { type: 'deposit', amount: 20 },
+  { type: 'withdrawal', amount: 5 },
+  { type: 'deposit', amount: 100 },
+  { type: 'withdrawal', amount: 25 },
+  { type: 'withdrawal', amount: 60 },
+];
+
+const traits = [
+  { color: 'yellow' },
+  { type: 'electric' },
+  { name: 'pikachu' },
+  { level: 15 },
+  { trainer: 'ash' },
+];
+
+const starterSum = 0;
+const sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, starterSum);
+console.log(sum);
+
+const product = numbers.reduce((currentNum, nextNum) => currentNum * nextNum);
+console.log(product);
+
+const intialValue = 0;
+const balance = account.reduce((total, transaction) => {
+  if (transaction.type === 'deposit') {
+    total += transaction.amount;
+  } else { total -= transaction.amount; }
+  return total;
+}, intialValue);
+console.log(balance);
+
+const result = { ...traits[0], ...traits[1], ...traits[2], ...traits[3], ...traits[4] };
+console.log(result);
