6f3c605bc52cc1b533402d98d3ad0cfd0ab5109c
Junou Lee <junoulee@gmail.com>
2023-02-01 03:43:25 +0000

    es6-promises - Junou Lee

--- es6-promises/index.js
+++ es6-promises/index.js
@@ -2,11 +2,10 @@ const takeAChance = require('./take-a-chance');
 
 const promise = takeAChance('Junou');
 
-promise
-  .then((message) => {
-    console.log(message);
-  })
+promise.then((message) => {
+  console.log(message);
+});
 
-  .catch((error) => {
-    console.error(error.message);
-  });
+promise.catch((error) => {
+  console.error(error.message);
+});
