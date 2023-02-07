e01fd5790b2497d21a72618de9220eaace464247
Junou Lee <junoulee@gmail.com>
2023-02-07 17:50:36 +0000

    react-stopwatch-component - Junou Lee

--- react-stopwatch-component/src/stopwatch.jsx
+++ react-stopwatch-component/src/stopwatch.jsx
@@ -3,31 +3,40 @@ import React, { useState } from 'react';
 export function StopWatch() {
   const [seconds, setSeconds] = useState(0);
   const [play, setPlay] = useState(false);
+  const [pauseID, setPauseID] = useState(null);
 
   function startTimer() {
     if (!play) {
-      setInterval(() => {
+
+      const count = setInterval(() => {
+        setPlay(true);
+        setPauseID(count);
         setSeconds((seconds) => seconds + 1);
       }, 1000);
     }
-    if (play) {
-      clearInterval(setSeconds(0));
-    }
+    playPause(pauseID);
   }
 
   function playPause() {
+    clearInterval(pauseID);
     setPlay(!play);
   }
+
   function controlButton() {
     if (play) {
       return 'fa-solid fa-pause pause';
     }
     return 'fa-solid fa-play play';
   }
-  // console.log(play);
+  function resetEverything() {
+    if (!play) {
+      clearInterval(setSeconds(0));
+    }
+  }
+
   return (
     <div className="circles">
-      <h3 onClick={startTimer}>{seconds}</h3>
+      <h3 onClick={resetEverything}>{seconds}</h3>
       <i onClick={() => {
         startTimer();
         playPause();
