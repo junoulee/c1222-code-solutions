b936682e17a3c92e76f6df3c84002183115f5e80
Junou Lee <junoulee@gmail.com>
2023-02-08 21:28:44 +0000

    react-app-drawer-component - Junou Lee

---
+++ react-app-drawer-component/src/app-drawer.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+
+export function AppDrawer() {
+  const [clicked, setClicked] = useState(false);
+
+  function handleClick() {
+    setClicked(!clicked);
+  }
+
+  return (
+    <>
+      <i className={clicked ? 'hidden' : 'fa-solid fa-bars burger' }
+        onClick={handleClick}></i>
+      <div className={clicked ? 'menu' : 'hidden'}>
+        <h3>Menu</h3>
+        <div className='links'>
+          <a onClick={handleClick}>About</a>
+        </div>
+        <div className='links'>
+          <a onClick={handleClick}>Get Started</a>
+        </div>
+        <div className='links'>
+          <a onClick={handleClick}>Sign In</a>
+        </div>
+      </div>
+      <div className={!clicked ? 'hidden' : 'overlay' }
+        onClick={handleClick} >
+      </div>
+    </>
+  );
+}
