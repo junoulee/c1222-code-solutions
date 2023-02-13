f911e1102fdaaa9ce65d4dee3d9ec3b3dc91c133
Junou Lee <junoulee@gmail.com>
2023-02-13 19:35:11 +0000

    error handling

--- fetch/main.js
+++ fetch/main.js
@@ -1,7 +1,13 @@
 fetch('https://pokeapi.co/api/v2/pokemon/1')
   .then((response) => response.json())
-  .then((data) => console.log(data));
+  .then((data) => console.log(data))
+  .catch((error) => {
+    console.error('Error:', error);
+  });
 
 fetch('https://jsonplaceholder.typicode.com/users')
   .then((response) => response.json())
-  .then((data) => console.log(data));
+  .then((data) => console.log(data))
+  .catch((error) => {
+    console.error('Error:', error);
+  });
