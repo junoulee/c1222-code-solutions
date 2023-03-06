f0314dd47b22519f1ee932f57694b3f5450b352e
Junou Lee <junoulee@gmail.com>
2023-03-06 23:45:03 +0000

    correct answers

--- big-o-notation/big-o.js
+++ big-o-notation/big-o.js
@@ -8,8 +8,8 @@ function uniqueLinear(words) {
     i < words.length;               // 2 * n = O(n)
     i++                             // 2 * n = O(n)
   ) {
-    const word = words[i];          // 3 * n = O(n)
-    if (!seen[word]) {              // 1 * n = O(n)
+    const word = words[i];          // 2 * n = O(n)
+    if (!seen[word]) {              // 2 * n = O(n)
       seen[word] = true;            // 2 * n = O(n)
       unique[unique.length] = word; // 2 * n = O(n)
     }
@@ -24,15 +24,15 @@ function uniqueQuadratic(words) {
     i < words.length;               // 2 * n = O(n)
     i++                             // 2 * n = O(n)
   ) {
-    const word = words[i];          // 3 * n = O(n)
+    const word = words[i];          // 2 * n = O(n)
     let isUnique = true;            // 1 * n = O(n)
     for (
       let c = 0;                    // 1 * n = O(n)
       c < i;                        // 2 * n^2 = O(n^2)
       c++                           // 2 * n^2 = O(n^2)
     ) {
-      const comparing = words[c];   // 3 * n^2 = O(n^2)
-      if (comparing === word) {     // 3 * n^2 = O(n^2)
+      const comparing = words[c];   // 2 * n^2 = O(n^2)
+      if (comparing === word) {     // 1 * n^2 = O(n^2)
         isUnique = false;           // 1 * n^2 = O(n^2)
       }
     }
